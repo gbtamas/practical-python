941de3c49483dbbfeeeebb7ae4224ad89a313fac
TAMAS Gabor <gabor.tamas@post.at>
2022-01-11 15:41:14 +0100

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+current_height = 100
+num_bounces = 0
+
+while num_bounces < 10:
+    num_bounces += 1
+    current_height = current_height * 0.6
+    print(num_bounces, round(current_height, 4))
